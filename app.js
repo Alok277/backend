665d61b81520fbdfe4cbe05095df0d4634431179
Alok <sonu.alokknp0512@gmail.com>
2024-08-11 14:34:15 +0530

    res html render

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(res)
-
-    res.end('Alok Ranjan');
+    if (req.url === '/home') {
+        res.write('Welocome home')
+    }
+    if (req.url === '/about') {
+        res.write('Welcome to About Us page')
+    }
+    if (req.url === './node') {
+        res.write('Welcome to my Node Js project')
+    }
+    res.end();
 })
 
 server.listen(4000)
